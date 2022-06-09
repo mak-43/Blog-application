8d7fab52bb9bb872eceffae1fc31996c49d15f43
Mak-43 <letsmailmak@gmail.com>
2022-06-09 06:32:58 +0600

    blogList

--- src/Components/Pages/BlogList/BlogList.js
+++ src/Components/Pages/BlogList/BlogList.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const BlogList = () => {
+    const [blog,setBlog]=useState([])
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setBlog(blog))
+    },[])
     return (
         <div>
             <h1>Blog List Page</h1>
